4051b197d0373f14ca24788e679954be893342eb
CHEN ZIJIAN <cc1556a@hotmail.com>
2020-10-11 01:06:47 +0800

    完成表单控制器objects

--- web-shell/controllers/objects.py
+++ web-shell/controllers/objects.py
@@ -1,6 +1,42 @@
 #
+import flask
+
+
+#
+from . import RPCRemote
+
+
+#
+from config import config
+
+
+#
+CORE_APIS_RPC_ADDR = config["CORE_APIS_RPC_ADDR"]
+CORE_APIS_RPC_PORT = int(config["CORE_APIS_RPC_PORT"])
+
+
+#
 def objects():
-    pass
+    method = flask.request.method
+    if method == "POST":
+        try:
+            form = flask.request.form
+            assert(type(form["project_name"]) == str)
+            assert(type(form["object_name"]) == str)
+            r = RPCRemote(CORE_APIS_RPC_ADDR, CORE_APIS_RPC_PORT)
+            result = r.pc("add_object")(form["object_name"], form["project_name"])
+            assert(type(result) == list)
+            assert(len(result) == 1)
+            assert(type(result[0] == list))
+            assert(len(result[0]) == 2)
+            if result[0][0] == 0:
+                return flask.redirect(flask.url_for("project", project_name=form["project_name"]))
+            else:
+                raise RuntimeError("RPC failure:", result[0][0], result[0][1])
+        except Exception as e:
+            return repr(e)
+    else:
+        flask.abort(405)
 
 
 #
