8861ae2a0b0d27061f29ecd27519ed7cc138c00b
CHEN ZIJIAN <cc1556a@hotmail.com>
2020-10-10 23:00:39 +0800

    修改路由描述格式

--- web-shell/controllers/__init__.py
+++ web-shell/controllers/__init__.py
@@ -16,5 +16,5 @@ route_descs = []
 def setup_routes(app:flask.Flask):
     for rd in route_descs:
         view = rd[0]
-        for url in rd[1]:
-            app.add_url_rule(view, url)
+        for url, methods in rd[1]:
+            app.add_url_rule(url, view_func=view, methods=methods)
