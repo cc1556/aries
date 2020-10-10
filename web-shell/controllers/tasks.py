0d4232c455746da8a01a56438e4588c1033bf0c6
CHEN ZIJIAN <cc1556a@hotmail.com>
2020-10-11 02:28:23 +0800

    完成表单控制器tasks

--- web-shell/controllers/tasks.py
+++ web-shell/controllers/tasks.py
@@ -1,6 +1,43 @@
 #
+import flask
+
+
+#
+from . import RPCRemote
+
+
+#
+from config import config
+
+
+#
+CORE_APIS_RPC_ADDR = config["CORE_APIS_RPC_ADDR"]
+CORE_APIS_RPC_PORT = int(config["CORE_APIS_RPC_PORT"])
+
+
+#
 def tasks():
-    pass
+    method = flask.request.method
+    if method == "POST":
+        try:
+            form = flask.request.form
+            assert(type(form["project_name"]) == str)
+            assert(type(form["object_name"]) == str)
+            assert(type(form["task_name"]) == str)
+            r = RPCRemote(CORE_APIS_RPC_ADDR, CORE_APIS_RPC_PORT)
+            result = r.pc("add_task")(form["task_name"], form["object_name"], form["project_name"])
+            assert(type(result) == list)
+            assert(len(result) == 1)
+            assert(type(result[0] == list))
+            assert(len(result[0]) == 2)
+            if result[0][0] == 0:
+                return flask.redirect(flask.url_for("object", project_name=form["project_name"], object_name=form["object_name"]))
+            else:
+                raise RuntimeError("RPC failure:", result[0][0], result[0][1])
+        except Exception as e:
+            return repr(e)
+    else:
+        flask.abort(405)
 
 
 #
