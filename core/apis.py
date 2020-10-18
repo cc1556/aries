fbe7afb2d5766762b79d0e2c71fa08ddeb6df0d1
CHEN ZIJIAN <cc1556a@hotmail.com>
2020-10-19 01:59:25 +0800

    完成coreAPI的create_merge和review_merge两函数

--- core/apis.py
+++ core/apis.py
@@ -1,5 +1,7 @@
 #
+import base64
 import fcntl
+import hashlib
 import os
 import os.path
 import re
@@ -10,6 +12,9 @@ import sqlite3
 
 
 #
+import Crypto.Hash.SHA512
+import Crypto.PublicKey.RSA
+import Crypto.Signature.PKCS1_v1_5
 import jinja2
 
 
@@ -542,3 +547,291 @@ def list_tasks(object:str, project:str):
     except Exception as e:
         return [-999, repr(e)]
     return [0, tasks]
+
+
+###
+### 创建merge review。
+### 参数：
+###     project：要操作的项目。
+###     object：要合并的目标。
+###     rev：要合并版本的hash值。
+###     message：提交信息。
+### 返回值：
+###     0：创建成功。
+###     -1：创建失败，project不存在。
+###     -2：创建失败，project中object不存在。
+###     -3：创建失败，rev不在object分支上。
+###
+def create_merge(project:str, object:str, rev:str, message:str):
+    try:
+        assert(len(rev) == 40)
+        lp = open(FLOCKS_DIR_PATH + "/aries.project." + project + ".lock", "wb")
+        lo = open(FLOCKS_DIR_PATH + "/aries.object." + project + "." + object + ".lock", "wb")
+        with lp, lo:
+            fcntl.flock(lp.fileno(), fcntl.LOCK_EX)
+            fcntl.flock(lo.fileno(), fcntl.LOCK_EX)
+            with sqlite3.connect(DATABASE_PATH) as conn:
+                c = conn.cursor()
+                # 查询project是否存在。    #########
+                r = c.execute(
+                        " \
+                        SELECT * \
+                            FROM projects \
+                            WHERE name=\"" + project + "\" \
+                        "
+                    )
+                for row in r:
+                    break
+                else:
+                    return [-1, None]
+                # 查询object是否存在。    #########
+                r = c.execute(
+                        " \
+                        SELECT * \
+                            FROM objects \
+                            WHERE \
+                                name=\"" + object + "\" \
+                                AND project=\"" + project + "\" \
+                        "
+                    )
+                for row in r:
+                    break
+                else:
+                    return [-2, None]
+                # 获取develop分支的头部。    #########
+                p = subprocess.run(
+                        ("cat refs/heads/develop").split(),
+                        cwd=GIT_DIR_PATH + "/" + project + ".git",
+                        capture_output=True,
+                        text=True
+                    )
+                if not p.stderr == "":
+                    raise RuntimeError("Command raise STDERR while creating README.md:\n" + "$ " + " ".join(p.args) + "\n" + p.stderr)
+                if not (len(p.stdout.strip()) == 40 and re.match(r"[0-9a-f]{40}", p.stdout.strip())):
+                    raise RuntimeError("Command print unexpected message to STDOUT:\n" + "$ " + " ".join(p.args) + "\n" + p.stdout)
+                develop_hashes = [re.match(r"[0-9a-f]{40}", p.stdout.strip()).string]
+                # 获取object分支的头部。    #########
+                p = subprocess.run(
+                        ("cat refs/heads/" + object).split(),
+                        cwd=GIT_DIR_PATH + "/" + project + ".git",
+                        capture_output=True,
+                        text=True
+                    )
+                if not p.stderr == "":
+                    raise RuntimeError("Command raise STDERR while creating README.md:\n" + "$ " + " ".join(p.args) + "\n" + p.stderr)
+                if not (len(p.stdout.strip()) == 40 and re.match(r"[0-9a-f]{40}", p.stdout.strip())):
+                    raise RuntimeError("Command print unexpected message to STDOUT:\n" + "$ " + " ".join(p.args) + "\n" + p.stdout)
+                object_hashes = [re.match(r"[0-9a-f]{40}", p.stdout.strip()).string]
+                # 查询object分支的起始位置。    #########
+                if not object_hashes[0] in develop_hashes:
+                    is_develop_at_initial_commit = False
+                    is_object_at_initial_commit = False
+                    while True:
+                        # 回溯develop分支。    #########
+                        if not is_develop_at_initial_commit:
+                            p = subprocess.run(
+                                    ("git cat-file -p " + develop_hashes[-1]).split(),
+                                    cwd=GIT_DIR_PATH + "/" + project + ".git",
+                                    capture_output=True,
+                                    text=True
+                                )
+                            if not p.stderr == "":
+                                raise RuntimeError("Git raise STDERR while creating README.md:\n" + "$ " + " ".join(p.args) + "\n" + p.stderr)
+                            if re.match(r"tree [0-9a-f]{40}\nparent [0-9a-f]{40}\n", p.stdout.strip()[0:94]):
+                                commit_hash = p.stdout.strip()[53:93]
+                                develop_hashes.append(commit_hash)
+                                if commit_hash in object_hashes:
+                                    break
+                            elif re.match(r"tree [0-9a-f]{40}\nauthor", p.stdout.strip()[0:52]):
+                                is_develop_at_initial_commit = True
+                            else:
+                                raise RuntimeError("Git print unexpected message to STDOUT:\n" + "$ " + " ".join(p.args) + "\n" + p.stdout)
+                        # 回溯object分支。    #########
+                        if not is_object_at_initial_commit:
+                            p = subprocess.run(
+                                    ("git cat-file -p " + object_hashes[-1]).split(),
+                                    cwd=GIT_DIR_PATH + "/" + project + ".git",
+                                    capture_output=True,
+                                    text=True
+                                )
+                            if not p.stderr == "":
+                                raise RuntimeError("Git raise STDERR while creating README.md:\n" + "$ " + " ".join(p.args) + "\n" + p.stderr)
+                            if re.match(r"tree [0-9a-f]{40}\nparent [0-9a-f]{40}\n", p.stdout.strip()[0:94]):
+                                commit_hash = p.stdout.strip()[53:93]
+                                object_hashes.append(commit_hash)
+                                if commit_hash in develop_hashes:
+                                    break
+                            elif re.match(r"tree [0-9a-f]{40}\nauthor", p.stdout.strip()[0:52]):
+                                is_object_at_initial_commit = True
+                            else:
+                                raise RuntimeError("Git print unexpected message to STDOUT:\n" + "$ " + " ".join(p.args) + "\n" + p.stdout)
+                        if is_develop_at_initial_commit and is_object_at_initial_commit:
+                            raise RuntimeError("develop分支和\"" + object + "\"分支没有共同祖先。")
+                else:
+                    commit_hash = object_hashes[0]
+                object_hashes = object_hashes[:object_hashes.index(commit_hash)+1]
+                # 查询rev是否在object分支上。    #########
+                if not rev in object_hashes:
+                    return [-3, None]
+                # 生成review id。    #########
+                message_b64 = base64.b64encode(message.encode("utf8")).decode("ascii")
+                review_base = (project + object + rev + message_b64 + develop_hashes[0]).encode("ascii")
+                h = hashlib.sha512()
+                h.update(review_base)
+                review_id = h.hexdigest()
+                # 写入数据库。    #########
+                c.execute(
+                        " \
+                        INSERT \
+                            INTO merges \
+                            VALUES ( \
+                                    \"" + review_id +  "\", \
+                                    \"" + project + "\", \
+                                    \"" + object + "\", \
+                                    \"" + rev + "\", \
+                                    \"" + develop_hashes[0] + "\", \
+                                    \"" + message_b64 + "\" \
+                                ) \
+                        "
+                    )
+                conn.commit()
+    except Exception as e:
+        return [-999, repr(e)]
+    return [0, review_id, develop_hashes[0]]
+
+
+###
+### Review merge。
+### 参数：
+###     review_id：Review id。
+###     signature：特权用户提供关于review_id的数字签名，表示审核通过。
+### 返回值：
+###     0：审核成功。
+###     -1：审核失败，review_id不存在。
+###     -2：审核失败，数据库中没有特权用户的记录。
+###     -3：审核失败，签名无效。
+###
+def review_merge(review_id:str, signature_b64:str):
+    try:
+        assert(len(review_id) == 128)
+        # 检查review_id是否存在。    #########
+        with sqlite3.connect(DATABASE_PATH) as conn:
+            c = conn.cursor()
+            r = c.execute(
+                    " \
+                    SELECT * \
+                        FROM merges \
+                        WHERE review_id=\"" + review_id + "\" \
+                    "
+                )
+            for row in r:
+                break
+            else:
+                return [-1, None]
+        # 取回review信息。    #########
+        project = row[1]
+        object = row[2]
+        object_rev = row[3]
+        develop_rev = row[4]
+        message_b64 = row[5]
+        message = base64.b64decode(message_b64.encode("ascii")).decode("utf8")
+        # 加锁。    #########
+        lp = open(FLOCKS_DIR_PATH + "/aries.project." + project + ".lock", "wb")
+        lo = open(FLOCKS_DIR_PATH + "/aries.object." + project + "." + object + ".lock", "wb")
+        with lp, lo:
+            fcntl.flock(lp.fileno(), fcntl.LOCK_EX)
+            fcntl.flock(lo.fileno(), fcntl.LOCK_EX)
+            # 检查review_id是否存在。    #########
+            with sqlite3.connect(DATABASE_PATH) as conn:
+                c = conn.cursor()
+                r = c.execute(
+                        " \
+                        SELECT * \
+                            FROM merges \
+                            WHERE review_id=\"" + review_id + "\" \
+                        "
+                    )
+                for row in r:
+                    break
+                else:
+                    return [-1, None]
+            # 确认锁定的review信息正确。    #########
+            assert(project == row[1])
+            assert(object == row[2])
+            assert(object_rev == row[3])
+            assert(develop_rev == row[4])
+            assert(message_b64 == row[5])
+            # 取回特权用户信息。    #########
+            with sqlite3.connect(DATABASE_PATH) as conn:
+                c = conn.cursor()
+                r = c.execute(
+                        " \
+                        SELECT * \
+                            FROM powerusers \
+                        "
+                    )
+                powerusers = [row for row in r]
+                if len(powerusers) == 0:
+                    return [-2, None]
+            # 验证签名。    #########
+            for user in powerusers:
+                uid = user[0]
+                uname = user[1]
+                upubkey = base64.b64decode(user[2])
+                h = Crypto.Hash.SHA512.new(review_id.encode("ascii"))
+                k = Crypto.PublicKey.RSA.importKey(upubkey)
+                s = Crypto.Signature.PKCS1_v1_5.new(k)
+                if s.verify(h, base64.b64decode(signature_b64)):
+                    break
+                else:
+                    return [-3, None]
+            # 获取rev对应的tree对象。    #########
+            p = subprocess.run(
+                    ("git cat-file -p " + object_rev).split(),
+                    cwd=GIT_DIR_PATH + "/" + project + ".git",
+                    capture_output=True,
+                    text=True
+                )
+            if not p.stderr == "":
+                raise RuntimeError("Git raise STDERR while creating README.md:\n" + "$ " + " ".join(p.args) + "\n" + p.stderr)
+            if not re.match(r"tree [0-9a-f]{40}\nparent [0-9a-f]{40}\n", p.stdout.strip()[0:94]):
+                raise RuntimeError("Git print unexpected message to STDOUT:\n" + "$ " + " ".join(p.args) + "\n" + p.stdout)
+            tree_hash = p.stdout.strip()[5:45]
+            # 创建merge commit对象。    #########
+            p = subprocess.run(
+                    ("git commit-tree " + tree_hash + " -p " + develop_rev + " -p " + object_rev).split(),
+                    cwd=GIT_DIR_PATH + "/" + project + ".git",
+                    capture_output=True,
+                    input=message,
+                    text=True
+                )
+            if not p.stderr == "":
+                raise RuntimeError("Git raise STDERR while creating README.md:\n" + "$ " + " ".join(p.args) + "\n" + p.stderr)
+            if not (len(p.stdout.strip()) == 40 and re.match(r"[0-9a-f]{40}", p.stdout.strip())):
+                raise RuntimeError("Git print unexpected message to STDOUT:\n" + "$ " + " ".join(p.args) + "\n" + p.stdout)
+            commit_hash = re.match(r"[0-9a-f]{40}", p.stdout.strip()).string
+            # 更新develop分支。    #########
+            p = subprocess.run(
+                    ("git update-ref refs/heads/develop " + commit_hash).split(),
+                    cwd=GIT_DIR_PATH + "/" + project + ".git",
+                    capture_output=True,
+                    text=True
+                )
+            if not p.stderr == "":
+                raise RuntimeError("Git raise STDERR while creating README.md:\n" + "$ " + " ".join(p.args) + "\n" + p.stderr)
+            if not p.stdout == "":
+                raise RuntimeError("Git print unexpected message to STDOUT:\n" + "$ " + " ".join(p.args) + "\n" + p.stdout)
+            # 从数据库中删除review。    #########
+            with sqlite3.connect(DATABASE_PATH) as conn:
+                c = conn.cursor()
+                r = c.execute(
+                        " \
+                        DELETE \
+                            FROM merges \
+                            WHERE review_id=\"" + review_id + "\" \
+                        "
+                    )
+                conn.commit()
+    except Exception as e:
+        return [-999, repr(e)]
+    return [0, None]
